8dd37724a0b073c160783eb0480c46f07fd4296d
kgaogelothorn <kgaogelothorn@gmail.com>
2024-05-27 23:26:59 +0200

    feat: created top cards

--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,17 @@ const App = () => (
     </ul>
 
     <div className="card card-one" style={{left: "160px", top: "60px"}}>
-          <img src={family} alt="Baby's birth" />
-          <div className="label">Baby’s birth</div>
-        </div>
+      <img src={family} alt="Baby's birth" />
+      <div className="label">Baby’s birth</div>
+    </div>
     <div className="card card-one" style={{left: "300px", bottom: "70px"}}>
-          <img src={holiday} alt="Holiday" />
-          <div className="label">Holiday</div>
-        </div>
+      <img src={holiday} alt="Holiday" />
+      <div className="label">Holiday</div>
+    </div>
     <div className="card card-one" style={{left: "720px", bottom: "200px"}}>
-          <img src={retire} alt="Retire" />
-          <div className="label">Retire</div>
-        </div>
+      <img src={retire} alt="Retire" />
+      <div className="label">Retire</div>
+    </div>
   </main>
 );
 
